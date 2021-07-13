8d62391446c26747fbae5e082236e8adceabfe58
XanderYe <XanderYe@outlook.com>
2021-07-13 23:30:39 +0800

    修改全局异常

--- springboot-archetype/src/main/resources/archetype-resources/src/main/java/exception/GlobalExceptionHandler.java
+++ springboot-archetype/src/main/resources/archetype-resources/src/main/java/exception/GlobalExceptionHandler.java
@@ -8,10 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -30,48 +26,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception e, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
         log.error(e.getMessage(), e);
-        return super.handleExceptionInternal(e, body, headers, status, request);
+        return new ResponseEntity<>(new ResultBean<>(status.value(), status.getReasonPhrase()), HttpStatus.OK);
     }
 
     @ExceptionHandler
     public ResponseEntity<Object> handleBusinessException(BusinessException e) {
-        return new ResponseEntity<>(new ResultBean(e.getCode(), e.getMessage()), HttpStatus.OK);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<Object> handleRuntimeException(RuntimeException e) {
-        log.error(e.getMessage(), e);
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<Object> handleException(Exception e) {
-        log.error(e.getMessage(), e);
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return new ResponseEntity<>(new ResultBean<>(1, "非法参数"), HttpStatus.OK);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        BindingResult bindingResult = ex.getBindingResult();
-        Map<String, String> map = new HashMap<>(16);
-        bindingResult.getFieldErrors().forEach(item -> {
-            map.put(item.getField(), item.getDefaultMessage());
-        });
-        return new ResponseEntity<>(new ResultBean<>(1, "非法参数", map), HttpStatus.OK);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        BindingResult bindingResult = ex.getBindingResult();
-        Map<String, String> map = new HashMap<>(16);
-        bindingResult.getFieldErrors().forEach(item -> {
-            map.put(item.getField(), item.getDefaultMessage());
-        });
-        return new ResponseEntity<>(new ResultBean<>(1, "非法参数", map), HttpStatus.OK);
+        return new ResponseEntity<>(new ResultBean<>(e.getCode(), e.getMessage()), HttpStatus.OK);
     }
 }
